8fcaf8e2f061bdb97389ad0656b99089f262d158
OseimuohanI <oseimuohanitua@outlook.com>
2025-12-15 03:06:22 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php error_reporting(E_ALL); ini_set('display_errors', 1); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Timer</title>
+</head>
+<body>
+    <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
+        <input type="number" placeholder="h" name="hour">
+        <input type="number" placeholder="m" name="minute">
+        <input type="number" placeholder="s" name="second">
+        <input type="submit" value="Start">
+    </form>
+    <?php
+    try {
+        $time = time();
+        (int) $hour = $_POST['hour'];
+        (int) $minute = $_POST['minute'];
+        (int) $second = $_POST['second'];
+    } catch (Throwable $e) {
+        echo "Welcome";
+    }
+
+        //try to append the isset functions::comma not functioning on keyboard
+        if (isset($hour) && isset($minute) && isset($second)){
+            $timeadd = 0;
+            $hour = $hour * 60 * 60;
+            $minute = $minute * 60;
+            $timeadd = $hour + $minute + $second;
+            echo $timeadd;
+        }
+    ?>
+</body>
+</html>
